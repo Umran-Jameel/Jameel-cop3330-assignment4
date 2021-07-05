bd79ccb8426486faf1236658718df7a803c86352
Umran Jameel <umranrazack02@mak-bwk-lmran.local>
2021-07-05 19:27:40 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/ApplicationController.java
@@ -0,0 +1,51 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Umran Jameel
+ */
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class ApplicationController {
+
+    public void addNewListClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeExistingListClicked(ActionEvent actionEvent) {
+    }
+
+    public void saveListToStorageClicked(ActionEvent actionEvent) {
+    }
+
+    public void saveAllListsClicked(ActionEvent actionEvent) {
+    }
+
+    public void loadListClicked(ActionEvent actionEvent) {
+    }
+
+    public void loadAllListsClicked(ActionEvent actionEvent) {
+    }
+
+    public void editTitleClicked(ActionEvent actionEvent) {
+    }
+
+    public void editDescriptionClicked(ActionEvent actionEvent) {
+    }
+
+    public void addItemClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeItemClicked(ActionEvent actionEvent) {
+    }
+
+    public void editDueDateClicked(ActionEvent actionEvent) {
+    }
+
+    public void markItemCompleteClicked(ActionEvent actionEvent) {
+    }
+
+}
---
+++ src/test/java/ucf/assignments/DisplayTest.java
@@ -0,0 +1,32 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DisplayTest {
+
+    @Test
+    void displayAllItems() {
+        // create a string with the expected items
+        // create a to do list with random items
+        // call the function, store the result (all of the items) in a array list (string)
+        // assertEquals(expected array list, actual)
+    }
+
+    @Test
+    void displayCompleted() {
+        // create a string with the expected items
+        // create a to do list with random items, some having the status of completed
+        // call the function, store the result (completed items) in a array list (string)
+        // assertEquals(expected array list, actual)
+    }
+
+    @Test
+    void displayIncomplete() {
+        // create a string with the expected items
+        // create a to do list with random items, some having the status of incomplete
+        // call the function, store the result (incomplete items) in a array list (string)
+        // assertEquals(expected array list, actual)
+    }
+}
